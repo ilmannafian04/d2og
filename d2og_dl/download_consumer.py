b8e99ce5b1dae58fc6d1f4d126dfecc6b04cfd58
Ilman Nafian <milmannafian04@gmail.com>
2020-06-04 15:35:36 +0700

    add download handler which will publish download progress

--- d2og_dl/download_consumer.py
+++ d2og_dl/download_consumer.py
@@ -1,7 +1,11 @@
+import errno
 import json
 import os
+import re
+import urllib.parse
 
 import pika
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -14,17 +18,64 @@ connection = pika.BlockingConnection(
     )
 )
 global_channel = connection.channel()
-exchange_name = f'{os.environ.get("NPM")}D'
-global_channel.exchange_declare(exchange_name, 'direct')
+npm = os.environ.get("NPM")
+global_channel.exchange_declare(f'{npm}D', 'direct')
 queue = global_channel.queue_declare(queue='download')
-global_channel.queue_bind(exchange=exchange_name, queue=queue.method.queue)
+global_channel.queue_bind(exchange=f'{npm}D', queue=queue.method.queue)
+global_channel.exchange_declare(f'{npm}T', 'topic')
+download_queue = global_channel.queue_declare(queue='progress.download')
+global_channel.queue_bind(exchange=f'{npm}T', queue=download_queue.method.queue)
+
+
+def get_filename(url, content_disposition):
+    if content_disposition is None:
+        if len(url.split('/')[-1]) > 0:
+            return url.split('/')[-1]
+        else:
+            return urllib.parse.quote(url, '')
+    fname = re.findall('filename=(.+)', content_disposition)
+    if len(fname) == 0:
+        return None
+    return fname[0]
 
 
 def download_handler(channel, method_frame, header_frame, body):
     message = body.decode('utf-8')
     message = json.loads(message)
     channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-    print(message)
+    response = requests.get(message['url'], stream=True)
+    filename = get_filename(message['url'], response.headers.get('content-disposition'))
+    download_folder = os.path.join(os.curdir, '..', 'media', message['key'])
+    if not os.path.exists(download_folder):
+        try:
+            os.makedirs(download_folder)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
+    elif os.path.exists(os.path.join(download_folder, filename)):
+        filename = f'{message["index"]}-{get_filename(message["url"], response.headers.get("content-disposition"))}'
+    with open(os.path.join(download_folder, filename), "wb") as file:
+        filesize = response.headers.get('content-length')
+        if filesize is None:
+            file.write(response.content)
+        else:
+            downloaded = 0
+            filesize = int(filesize)
+            for data in response.iter_content(chunk_size=filesize // 1000):
+                downloaded += len(data)
+                file.write(data)
+                channel.basic_publish(
+                    f'{npm}T',
+                    'progress.download',
+                    json.dumps(
+                        {
+                            'key': message['key'],
+                            'index': message['index'],
+                            'progress': round(downloaded * 100 / filesize, 2)
+                        },
+                        separators=(',', ':')
+                    )
+                )
 
 
 global_channel.basic_consume('download', download_handler)
